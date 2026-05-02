8845556469f7c0b9b21a15244ba5aa3dc9995dd0
Huy Dong <email_cua_huy@gmail.com>
2026-05-03 00:11:04 +0700

    Thêm dòng import file CSS để fix lỗi giao diện khổng lồ

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import './index.css' // Đây chính là "linh hồn" giao diện mà chúng ta đã quên!
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
